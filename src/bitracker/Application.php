a64baff6977df0853884ec9fe487f0fd64cfe602
GaalexxC <gacornell@verizon.net>
2018-02-14 11:06:38 -0500

    License

--- src/bitracker/Application.php
+++ src/bitracker/Application.php
@@ -1,14 +1,31 @@
 <?php
 /**
  * @brief		BitTracker Application Class
- * @author		Gary Cornell
- * @copyright		(c) <a href='https://www.devcu.com'>devCU Software Development</a>
- * @license		https://www.invisioncommunity.com/legal/standards/
- * @package		Invision Community
- * @subpackage		BitTracker
- * @since		11 FEB 2018
+ * @author		Gary Cornell for devCU Software Open Source Projects
+ * @copyright	(c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license		GNU General Public License v3.0
+ * @package		Invision Community Suite 4.2x
+ * @subpackage	BitTracker
+ * @version     1.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @Created		11 FEB 2018
+ * @Updated     14 FEB 2018
+ *
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
  */
- 
+
 namespace IPS\bitracker;
 
 /**
