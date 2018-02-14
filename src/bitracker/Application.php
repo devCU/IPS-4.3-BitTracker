c8d3489d884ded70a6e241f3f6165b422c7e94cb
GaalexxC <gacornell@verizon.net>
2018-02-14 11:08:18 -0500

    Formatting
    Github Hello

--- src/bitracker/Application.php
+++ src/bitracker/Application.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * @brief		BitTracker Application Class
- * @author		Gary Cornell for devCU Software Open Source Projects
- * @copyright	(c) <a href='https://www.devcu.com'>devCU Software Development</a>
- * @license		GNU General Public License v3.0
- * @package		Invision Community Suite 4.2x
+ * @brief       BitTracker Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.2x
  * @subpackage	BitTracker
  * @version     1.0.0 Beta 1
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
- * @Created		11 FEB 2018
+ * @Created     11 FEB 2018
  * @Updated     14 FEB 2018
  *
+ *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
  *    it under the terms of the GNU General Public License as published by       
  *    the Free Software Foundation, either version 3 of the License, or          
