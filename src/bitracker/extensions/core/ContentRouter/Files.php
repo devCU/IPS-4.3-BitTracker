afc2131c7ac4e9a6fd3fb0e9ab6db8e2689d073a
GaalexxC <gacornell@verizon.net>
2018-02-15 12:54:44 -0500

    ContentRouter

---
+++ src/bitracker/extensions/core/ContentRouter/Files.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @brief       BitTracker Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.2x
+ * @subpackage	BitTracker
+ * @version     1.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @Created     11 FEB 2018
+ * @Updated     15 FEB 2018
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\ContentRouter;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * @brief	Content Router extension: BitTracker
+ */
+class _Files
+{
+	/**
+	 * @brief	Content Item Classes
+	 */
+	public $classes = array();
+	
+	/**
+	 * Constructor
+	 *
+	 * @param	\IPS\Member|IPS\Member\Group|NULL	$member		If checking access, the member/group to check for, or NULL to not check access
+	 * @return	void
+	 */
+	public function __construct( $member = NULL )
+	{
+		if ( $member === NULL or $member->canAccessModule( \IPS\Application\Module::get( 'bitracker', 'bitracker', 'front' ) ) )
+		{
+			$this->classes[] = 'IPS\bitracker\File';
+		}
+	}
+}
