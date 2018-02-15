ffcea287b51f515654ec70260c761f6964dc5d1a
GaalexxC <gacornell@verizon.net>
2018-02-15 12:34:46 -0500

    Nexus

---
+++ src/bitracker/extensions/nexus/Item/File.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * @brief       BitTracker Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.2x
+ * @subpackage	BitTracker
+ * @version     1.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @Created     11 FEB 2018
+ * @Updated     14 FEB 2018
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\nexus\Item;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * File
+ */
+class _File extends \IPS\nexus\Invoice\Item\Purchase
+{
+	/**
+	 * @brief	Application
+	 */
+	public static $application = 'bitracker';
+	
+	/**
+	 * @brief	Application
+	 */
+	public static $type = 'file';
+	
+	/**
+	 * @brief	Icon
+	 */
+	public static $icon = 'download';
+	
+	/**
+	 * @brief	Title
+	 */
+	public static $title = 'file';
+	
+	/**
+	 * Image
+	 *
+	 * @return |IPS\File|NULL
+	 */
+	public function image()
+	{
+		try
+		{
+			return \IPS\bitracker\File::load( $this->id )->primary_screenshot;
+		}
+		catch ( \Exception $e )
+		{
+			return NULL;
+		}
+	}
+	
+	/**
+	 * Image
+	 *
+	 * @param	\IPS\nexus\Purchase	$purchase	The purchase
+	 * @return |IPS\File|NULL
+	 */
+	public static function purchaseImage( \IPS\nexus\Purchase $purchase )
+	{
+		try
+		{			
+			return \IPS\bitracker\File::load( $purchase->item_id )->primary_screenshot;
+		}
+		catch ( \Exception $e )
+		{
+			return NULL;
+		}
+	}
+	
+	/**
+	 * Get Client Area Page HTML
+	 *
+	 * @return	array( 'packageInfo' => '...', 'purchaseInfo' => '...' )
+	 */
+	public static function clientAreaPage( \IPS\nexus\Purchase $purchase )
+	{
+		try
+		{
+			$file = \IPS\bitracker\File::load( $purchase->item_id );
+			
+			return array( 'packageInfo' => \IPS\Theme::i()->getTemplate( 'nexus', 'bitracker' )->fileInfo( $file ), 'purchaseInfo' => \IPS\Theme::i()->getTemplate( 'nexus', 'bitracker' )->filePurchaseInfo( $file ) );
+		}
+		catch ( \OutOfRangeException $e ) { }
+		
+		return NULL;
+	}
+	
+	/**
+	 * Get ACP Page HTML
+	 *
+	 * @return	string
+	 */
+	public static function acpPage( \IPS\nexus\Purchase $purchase )
+	{
+		try
+		{
+			$file = \IPS\bitracker\File::load( $purchase->item_id );
+			return \IPS\Theme::i()->getTemplate( 'nexus', 'bitracker' )->fileInfo( $file );
+		}
+		catch ( \OutOfRangeException $e ) { }
+		
+		return NULL;
+	}
+	
+	/**
+	 * URL
+	 *
+	 * @return |IPS\Http\Url|NULL
+	 */
+	public function url()
+	{
+		try
+		{
+			return \IPS\bitracker\File::load( $this->id )->url();
+		}
+		catch ( \OutOfRangeException $e )
+		{
+			return NULL;
+		}
+	}
+	
+	/**
+	 * ACP URL
+	 *
+	 * @return |IPS\Http\Url|NULL
+	 */
+	public function acpUrl()
+	{
+		return $this->url();
+	}
+	
+	/** 
+	 * Get renewal payment methods IDs
+	 *
+	 * @param	\IPS\nexus\Purchase	$purchase	The purchase
+	 * @return	array|NULL
+	 */
+	public static function renewalPaymentMethodIds( \IPS\nexus\Purchase $purchase )
+	{
+		if ( \IPS\Settings::i()->bit_nexus_gateways )
+		{
+			return explode( ',', \IPS\Settings::i()->bit_nexus_gateways );
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+}
