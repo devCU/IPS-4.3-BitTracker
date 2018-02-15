126f08d6ddc65892d805e6547cc522b76606074e
GaalexxC <gacornell@verizon.net>
2018-02-15 16:07:23 -0500

    MemberForm

---
+++ src/bitracker/extensions/core/MemberForm/Bitracker.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @brief       BitTracker Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.2x
+ * @subpackage	BitTracker
+ * @version     1.0.0 Beta 1
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
+ * @Created     11 FEB 2018
+ * @Updated     15 FEB 2018
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\bitracker\extensions\core\MemberForm;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Admin CP Member Form
+ */
+class _Bitracker
+{
+	/**
+	 * Process Form
+	 *
+	 * @param	\IPS\Helpers\Form		$form	The form
+	 * @param	\IPS\Member				$member	Existing Member
+	 * @return	void
+	 */
+	public function process( &$form, $member )
+	{		
+		$form->addMessage( "<a href='" . \IPS\Http\Url::internal( 'app=bitracker&module=stats&controller=member&do=bitracker&id=' . $member->member_id ) . "'>" . \IPS\Member::loggedIn()->language()->addToStack('bitracker_stats') . '</a>', '', FALSE );
+		$form->add( new \IPS\Helpers\Form\YesNo( 'bit_block_submissions', !$member->bit_block_submissions ) );
+	}
+	
+	/**
+	 * Save
+	 *
+	 * @param	array				$values	Values from form
+	 * @param	\IPS\Member			$member	The member
+	 * @return	void
+	 */
+	public function save( $values, &$member )
+	{
+		$member->bit_block_submissions = !$values['bit_block_submissions'];	
+	}
+}
