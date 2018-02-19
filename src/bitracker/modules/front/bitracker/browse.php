355b1184715e1237d34092edd3c0164e8a34068f
GaalexxC <gacornell@verizon.net>
2018-02-19 17:28:22 -0500

    Versioning

--- src/bitracker/modules/front/bitracker/browse.php
+++ src/bitracker/modules/front/bitracker/browse.php
@@ -10,7 +10,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     16 FEB 2018
+ * @Updated     19 FEB 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
