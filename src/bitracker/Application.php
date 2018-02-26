fb01dd5c920ad5cbbfed87146a0e14955d69eec0
GaalexxC <gacornell@verizon.net>
2018-02-26 17:21:26 -0500

    ACP
    Group Permissions
    Removed Legacy
    ICON
    Cleanup

--- src/bitracker/Application.php
+++ src/bitracker/Application.php
@@ -10,7 +10,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     14 FEB 2018
+ * @Updated     26 FEB 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -41,7 +41,7 @@ class _Application extends \IPS\Application
 	 */
 	public function init()
 	{
-		/* If the viewing member cannot view the board (ex: guests must login first), then send a 404 Not Found header here, before the Login page shows in the dispatcher */
+		/* Can view bitracker app? */
 		if ( !\IPS\Member::loggedIn()->group['g_view_board'] and ( \IPS\Request::i()->module == 'bitracker' and \IPS\Request::i()->controller == 'browse' and \IPS\Request::i()->do == 'rss' ) )
 		{
 			\IPS\Output::i()->error( 'node_error', '2D220/1', 404, '' );
@@ -54,6 +54,39 @@ class _Application extends \IPS\Application
 			\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'bitracker_responsive.css', 'bitracker', 'front' ) );
 		}
 	}
+    
+	/**
+	 * Group Permissions
+	 *
+	 * @return void
+	 */
+	public function installOther()
+	{
+	    /* Give non guests what permissions they need */
+		foreach( \IPS\Member\Group::groups( TRUE, FALSE ) as $group )
+		{
+		    /* P */
+            $group->g_bit_add_torrent     = TRUE;
+            $group->g_bit_edit_torrent    = TRUE;
+            $group->g_bit_add_comments    = TRUE;
+            $group->g_bit_edit_comments   = TRUE;
+            $group->g_bit_rate_torrent    = TRUE;                        
+            $group->save();
+            
+		    /* VIP */
+            if( $group->g_access_offline )
+            {
+                $group->g_bit_delete_torrent      = TRUE;
+                $group->g_bit_delete_comments     = TRUE;
+                $group->g_bit_m_edit_torrent      = TRUE;
+                $group->g_bit_m_delete_torrent    = TRUE;
+                $group->g_bit_m_edit_comments     = TRUE;   
+                $group->g_bit_m_delete_comments   = TRUE;                 
+                $group->g_bit_m_manage            = TRUE;                                      
+                $group->save();
+            }            
+		}
+	}  
 
 	/**
 	 * [Node] Get Icon for tree
@@ -63,7 +96,7 @@ class _Application extends \IPS\Application
 	 */
 	protected function get__icon()
 	{
-		return 'bitrack';
+		return 'cloud';
 	}
 	
 	/**
@@ -97,29 +130,5 @@ class _Application extends \IPS\Application
 			'browseTabsEnd'	=> array(),
 			'activityTabs'	=> array()
 		);
-	}
-	
-	/**
-	 * Perform some legacy URL parameter conversions
-	 *
-	 * @return	void
-	 */
-	public function convertLegacyParameters()
-	{
-		if ( isset( \IPS\Request::i()->showfile ) AND is_numeric( \IPS\Request::i()->showfile ) )
-		{
-			try
-			{
-				$file = \IPS\bitracker\File::loadAndCheckPerms( \IPS\Request::i()->showfile );
-				
-				\IPS\Output::i()->redirect( $file->url() );
-			}
-			catch( \OutOfRangeException $e ) {}
-		}
-
-		if ( isset( \IPS\Request::i()->module ) AND \IPS\Request::i()->module == 'post' AND isset( \IPS\Request::i()->controller ) AND \IPS\Request::i()->controller == 'submit' )
-		{
-			\IPS\Output::i()->redirect( \IPS\Http\Url::internal( "app=bitracker&module=bitracker&controller=submit", "front", "bitracker_submit" ) );
-		}
 	}
 }
