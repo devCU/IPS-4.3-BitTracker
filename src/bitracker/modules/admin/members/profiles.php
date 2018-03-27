c03f213f834c0563ecde1d6d8295d4d726656893
Gary Cornell <gary@devcu.com>
2018-03-27 19:25:17 -0400

    Profile :: Advanced
    Advanced Settings Enable?
    When enabled members have access to settings in their control panel to limit access to the profiles tab. For example they can choose to hide their uploads and/or downloads
    
    Added lang bits and settings

--- src/bitracker/modules/admin/members/profiles.php
+++ src/bitracker/modules/admin/members/profiles.php
@@ -10,7 +10,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     26 MAR 2018
+ * @Updated     27 MAR 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -97,6 +97,7 @@ class _profiles extends \IPS\Dispatcher\Controller
 
         $form->addTab( 'advanced_settings' );  
         $form->addHeader( 'advanced_settings' );
+        $form->add( new \IPS\Helpers\Form\YesNo( 'bit_profile_adv_enable', \IPS\Settings::i()->bit_profile_adv_enable, FALSE, array(), NULL, NULL, NULL, 'bit_profile_adv_enable' ) );
 
 		/* Save values */
 		if ( $values = $form->values() )
