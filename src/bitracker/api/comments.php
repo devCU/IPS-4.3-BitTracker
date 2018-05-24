62ad09d14fd551d049d8bf6fcb36019bc2101510
Gary Cornell <gary@devcu.com>
2018-05-24 14:39:06 -0400

    Beta2 API
    Comments

--- src/bitracker/api/comments.php
+++ src/bitracker/api/comments.php
@@ -4,13 +4,13 @@
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.2x
+ * @package     Invision Community Suite 4.2x/4.3x
  * @subpackage	BitTracker
- * @version     1.0.0 Beta 1
+ * @version     1.0.0 Beta 2
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     27 FEB 2018
+ * @Updated     24 MAY 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -50,6 +50,7 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * GET /bitracker/comments
 	 * Get list of comments
 	 *
+	 * @note		For requests using an OAuth Access Token for a particular member, only comments the authorized user can view will be included
 	 * @apiparam	string	categories		Comma-delimited list of category IDs
 	 * @apiparam	string	authors			Comma-delimited list of member IDs - if provided, only topics started by those members are returned
 	 * @apiparam	int		locked			If 1, only comments from events which are locked are returned, if 0 only unlocked
@@ -58,6 +59,7 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * @apiparam	string	sortBy			What to sort by. Can be 'date', 'title' or leave unspecified for ID
 	 * @apiparam	string	sortDir			Sort direction. Can be 'asc' or 'desc' - defaults to 'asc'
 	 * @apiparam	int		page			Page number
+	 * @apiparam	int		perPage			Number of results per page - defaults to 25
 	 * @return		\IPS\Api\PaginatedResponse<IPS\bitracker\File\Comment>
 	 */
 	public function GETindex()
@@ -70,14 +72,24 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * View information about a specific comment
 	 *
 	 * @param		int		$id			ID Number
-	 * @throws		2D304/1	INVALID_ID	The comment ID does not exist
-	 * @return		\IPS\bitracker\File\Comment
+	 * @throws		2D304/1	INVALID_ID	The comment ID does not exist or the authorized user does not have permission to view it
+	 * @return		\IPS\downloads\File\Comment
 	 */
 	public function GETitem( $id )
 	{
 		try
 		{
-			return new \IPS\Api\Response( 200, \IPS\bitracker\File\Comment::load( $id )->apiOutput() );
+			$class = $this->class;
+			if ( $this->member )
+			{
+				$object = $class::loadAndCheckPerms( $id, $this->member );
+			}
+			else
+			{
+				$object = $class::load( $id );
+			}
+			
+			return new \IPS\Api\Response( 200, $object->apiOutput( $this->member ) );
 		}
 		catch ( \OutOfRangeException $e )
 		{
@@ -89,21 +101,23 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * POST /bitracker/comments
 	 * Create a comment
 	 *
+	 * @note	For requests using an OAuth Access Token for a particular member, any parameters the user doesn't have permission to use are ignored (for example, hidden will only be honoured if the authentictaed user has permission to hide content).
 	 * @reqapiparam	int			file				The ID number of the file the comment is for
-	 * @reqapiparam	int			author				The ID number of the member making the comment (0 for guest)
+	 * @reqapiparam	int			author				The ID number of the member making the comment (0 for guest). Required for requests made using an API Key or the Client Credentials Grant Type. For requests using an OAuth Access Token for a particular member, that member will always be the author
 	 * @apiparam	string		author_name			If author is 0, the guest name that should be used
-	 * @reqapiparam	string		content				The comment content as HTML (e.g. "<p>This is a comment.</p>")
-	 * @apiparam	datetime	date				The date/time that should be used for the comment date. If not provided, will use the current date/time
-	 * @apiparam	string		ip_address			The IP address that should be stored for the comment. If not provided, will use the IP address from the API request
+	 * @reqapiparam	string		content				The comment content as HTML (e.g. "<p>This is a comment.</p>"). Will be sanatized for requests using an OAuth Access Token for a particular member; will be saved unaltered for requests made using an API Key or the Client Credentials Grant Type. 
+	 * @apiparam	datetime	date				The date/time that should be used for the comment date. If not provided, will use the current date/time. Ignored for requests using an OAuth Access Token for a particular member
+	 * @apiparam	string		ip_address			The IP address that should be stored for the comment. If not provided, will use the IP address from the API request. Ignored for requests using an OAuth Access Token for a particular member
 	 * @apiparam	int			hidden				0 = unhidden; 1 = hidden, pending moderator approval; -1 = hidden (as if hidden by a moderator)
 	 * @throws		2D304/2		INVALID_ID	The comment ID does not exist
 	 * @throws		1D304/3		NO_AUTHOR	The author ID does not exist
 	 * @throws		1D304/4		NO_CONTENT	No content was supplied
+	 * @throws		2D304/8		NO_PERMISSION		The authorized user does not have permission to comment on that file
 	 * @return		\IPS\bitracker\File\Comment
 	 */
 	public function POSTindex()
 	{
-		/* Get topic */
+		/* Get file */
 		try
 		{
 			$file = \IPS\bitracker\File::load( \IPS\Request::i()->file );
@@ -114,18 +128,29 @@ class _comments extends \IPS\Content\Api\CommentController
 		}
 		
 		/* Get author */
-		if ( \IPS\Request::i()->author )
+		if ( $this->member )
 		{
-			$author = \IPS\Member::load( \IPS\Request::i()->author );
-			if ( !$author->member_id )
+			if ( !$file->canComment( $this->member ) )
 			{
-				throw new \IPS\Api\Exception( 'NO_AUTHOR', '1D304/3', 404 );
+				throw new \IPS\Api\Exception( 'NO_PERMISSION', '2D304/8', 403 );
 			}
+			$author = $this->member;
 		}
 		else
 		{
-			$author = new \IPS\Member;
-			$author->name = \IPS\Request::i()->author_name;
+			if ( \IPS\Request::i()->author )
+			{
+				$author = \IPS\Member::load( \IPS\Request::i()->author );
+				if ( !$author->member_id )
+				{
+					throw new \IPS\Api\Exception( 'NO_AUTHOR', '1D304/3', 404 );
+				}
+			}
+			else
+			{
+				$author = new \IPS\Member;
+				$author->name = \IPS\Request::i()->author_name;
+			}
 		}
 		
 		/* Check we have a post */
@@ -142,13 +167,15 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * POST /bitracker/comments/{id}
 	 * Edit a comment
 	 *
+	 * @note		For requests using an OAuth Access Token for a particular member, any parameters the user doesn't have permission to use are ignored (for example, hidden will only be honoured if the authentictaed user has permission to hide content).
 	 * @param		int			$id				ID Number
-	 * @apiparam	int			author			The ID number of the member making the comment (0 for guest)
+	 * @apiparam	int			author			The ID number of the member making the comment (0 for guest). Ignored for requests using an OAuth Access Token for a particular member.
 	 * @apiparam	string		author_name		If author is 0, the guest name that should be used
-	 * @apiparam	string		content			The comment content as HTML (e.g. "<p>This is a comment.</p>")
+	 * @apiparam	string		content			The comment content as HTML (e.g. "<p>This is a comment.</p>"). Will be sanatized for requests using an OAuth Access Token for a particular member; will be saved unaltered for requests made using an API Key or the Client Credentials Grant Type. 
 	 * @apiparam	int			hidden				1/0 indicating if the topic should be hidden
-	 * @throws		2D304/5		INVALID_ID			The comment ID does not exist
+	 * @throws		2D304/5		INVALID_ID			The comment ID does not exist or the authorized user does not have permission to view it
 	 * @throws		1D304/6		NO_AUTHOR			The author ID does not exist
+	 * @throws		2D304/9		NO_PERMISSION		The authorized user does not have permission to edit the comment
 	 * @return		\IPS\bitracker\File\Comment
 	 */
 	public function POSTitem( $id )
@@ -157,6 +184,14 @@ class _comments extends \IPS\Content\Api\CommentController
 		{
 			/* Load */
 			$comment = \IPS\bitracker\File\Comment::load( $id );
+			if ( $this->member and !$comment->canView( $this->member ) )
+			{
+				throw new \OutOfRangeException;
+			}
+			if ( $this->member and !$comment->canEdit( $this->member ) )
+			{
+				throw new \IPS\Api\Exception( 'NO_PERMISSION', '2D304/9', 403 );
+			}
 						
 			/* Do it */
 			try
@@ -179,14 +214,21 @@ class _comments extends \IPS\Content\Api\CommentController
 	 * Deletes a comment
 	 *
 	 * @param		int			$id			ID Number
-	 * @throws		2D304/7		INVALID_ID	The comment ID does not exist
+	 * @throws		2D304/7		INVALID_ID		The comment ID does not exist
+	 * @throws		2D304/A		NO_PERMISSION	The authorized user does not have permission to delete the comment
 	 * @return		void
 	 */
 	public function DELETEitem( $id )
 	{
 		try
 		{			
-			\IPS\bitracker\File\Comment::load( $id )->delete();
+			$class = $this->class;
+			$object = $class::load( $id );
+			if ( $this->member and !$object->canDelete( $this->member ) )
+			{
+				throw new \IPS\Api\Exception( 'NO_PERMISSION', '2D304/A', 403 );
+			}
+			$object->delete();
 			
 			return new \IPS\Api\Response( 200, NULL );
 		}
