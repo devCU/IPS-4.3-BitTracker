d65096be631506e32201ac63b1793ec8b44e7845
Gary Cornell <gary@devcu.com>
2018-05-24 19:35:15 -0400

    APP | HOOKS
    Re-factored
    IPS 4.3x compatibility

--- src/bitracker/hooks/Forums.php
+++ src/bitracker/hooks/Forums.php
@@ -9,19 +9,39 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 class bitracker_hook_Forums extends _HOOK_CLASS_
 {
 	/**
-	 * Delete Record
+	 * [Node] Get buttons to display in tree
 	 *
-	 * @return	void
+	 * @param	string	$url		Base URL
+	 * @param	bool	$subnode	Is this a subnode?
+	 * @return	array
 	 */
-	public function delete()
+	public function getButtons( $url, $subnode=FALSE )
 	{
-		foreach( new \IPS\Patterns\ActiveRecordIterator( \IPS\Db::i()->select( '*', 'bitracker_categories', array( "cforum_id=?", $this->_id ) ), 'IPS\bitracker\Category' ) AS $category )
+		$buttons = parent::getButtons( $url, $subnode );
+
+		if ( isset( $buttons['delete'] ) AND $this->isUsedByABitrackerCategory() )
 		{
-			$category->forum_id = 0;
-			$category->save();
+			unset( $buttons['delete']['data'] );
 		}
-		
-		return call_user_func_array( 'parent::delete', func_get_args() );
+
+		return $buttons;
+	}
+
+	/**
+	 * Is this Forum used by any bitracker app category ?
+	 *
+	 * @return bool|\IPS\bitracker\Category
+	 */
+	public function isUsedByABitrackerCategory()
+	{
+		foreach( new \IPS\Patterns\ActiveRecordIterator( \IPS\Db::i()->select( '*', 'bitracker_categories' ), 'IPS\bitracker\Category' ) AS $category )
+		{
+			if ( $category->forum_id and $category->forum_id == $this->id )
+			{
+				return $category;
+			}
+		}
+		return FALSE;
 	}
 
 }
