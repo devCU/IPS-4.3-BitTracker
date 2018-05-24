9758523b62191a0ba3d59cac9fc1e8fdcbaebd59
Gary Cornell <gary@devcu.com>
2018-05-24 14:38:51 -0400

    Beta2 API
    Categories

--- src/bitracker/api/categories.php
+++ src/bitracker/api/categories.php
@@ -4,13 +4,13 @@
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.2x
+ * @package     Invision Community Suite 4.2x/4.3x
  * @subpackage	BitTracker
- * @version     1.0.0 Beta 1
+ * @version     1.0.0 Beta 2
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     27 FEB 2018
+ * @Updated     24 MAY 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -93,7 +93,7 @@ class _categories extends \IPS\Node\Api\NodeController
 	 */
 	public function POSTindex()
 	{
-		return $this->_create();
+		return new \IPS\Api\Response( 201, $this->_create()->apiOutput( $this->member ) );
 	}
 
 	/**
@@ -119,9 +119,8 @@ class _categories extends \IPS\Node\Api\NodeController
 	{
 		$class = $this->class;
 		$category = $class::load( $id );
-		$category = $this->_createOrUpdate( $category );
-
-		return $category;
+		
+		return new \IPS\Api\Response( 201, $this->_createOrUpdate( $category )->apiOutput( $this->member ) );
 	}
 
 	/**
