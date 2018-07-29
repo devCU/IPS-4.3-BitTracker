b4a80c0f4e43e750084067cf0aeb135ef0bcb4ce
Gary Cornell <gary@devcu.com>
2018-07-28 21:14:32 -0400

    IPS 4.3.5 Upgrade
    Legacy node, useless for a new beta but....

--- src/bitracker/Application.php
+++ src/bitracker/Application.php
@@ -10,7 +10,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/ips4bt/
  * @Created     11 FEB 2018
- * @Updated     24 MAY 2018
+ * @Updated     28 JULY 2018
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -110,7 +110,7 @@ class _Application extends \IPS\Application
 		{
 			try
 			{
-				$file = \IPS\downloads\File::loadAndCheckPerms( \IPS\Request::i()->showfile );
+				$file = \IPS\bitracker\File::loadAndCheckPerms( \IPS\Request::i()->showfile );
 				
 				\IPS\Output::i()->redirect( $file->url() );
 			}
