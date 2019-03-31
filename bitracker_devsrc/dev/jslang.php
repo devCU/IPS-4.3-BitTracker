b3497d4dfc382bc55cd4e2aca8bd6c846e9eb850
GaalexxC <gary@devcu.com>
2019-03-31 05:49:55 -0400

    dev Lang

---
+++ bitracker_devsrc/dev/jslang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = array(
+
+'makePrimaryScreenshot'	=> "Make this the primary screenshot",
+'removeScreenshot'	=> "Remove this screenshot",
+'rebuild_bitracker'	=> "Rebuild Torrents",
+'siteapi_settings'	=> "API Settings",
+'siteapi_enabled'	=> "Enable?",
+'siteapi_name'	=> "Site Name",
+'siteapi_host'	=> "Host URL",
+'siteapi_url'	=> "Example URL",
+'siteapi_oembed'	=> "API URL"
+
+);
