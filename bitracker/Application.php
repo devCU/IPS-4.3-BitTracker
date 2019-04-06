04ab11f848c12b3b8cf9a43bba9f9cd639a6528d
GaalexxC <gary@devcu.com>
2019-04-06 05:11:31 -0400

    FontAwesome 5.8x

--- bitracker/Application.php
+++ bitracker/Application.php
@@ -13,7 +13,7 @@
  * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
  * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
  * @Created     11 FEB 2018
- * @Updated     05 APR 2019
+ * @Updated     06 APR 2019
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -66,7 +66,7 @@ class _Application extends \IPS\Application
 	 */
 	protected function get__icon()
 	{
-		return 'cloud-upload';
+		return 'cloud-upload-alt';
 	}
 	
 	/**
