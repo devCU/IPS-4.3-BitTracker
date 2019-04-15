695035545f37a502f74915f626fd3b3c46f80f40
GaalexxC <gary@devcu.com>
2019-04-15 11:05:40 -0400

    announce/scrape
    initial commit

---
+++ bitracker_devsrc/modules/front/client/scrape.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       BitTracker Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.2x/4.3x
+ * @subpackage	BitTracker
+ * @version     1.0.3
+ * @source      https://github.com/GaalexxC/IPS-4.2-BitTracker
+ * @Issue Trak  https://www.devcu.com/forums/devcu-tracker/
+ * @Created     11 FEB 2018
+ * @Updated     15 APR 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
