4a9fc77fe92a33f4d549a2f4beebcd194a0d1375
GaalexxC <gary@devcu.com>
2019-04-16 19:57:20 -0400

    Stable 1.0.3

--- bitracker_devsrc/modules/admin/about/version.php
+++ bitracker_devsrc/modules/admin/about/version.php
@@ -33,7 +33,7 @@
 namespace IPS\bitracker\modules\admin\about;
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
-if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 {
 	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
 	exit;
